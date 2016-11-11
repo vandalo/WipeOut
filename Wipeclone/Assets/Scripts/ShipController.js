c87e32d30fe4eefb871b78a39926e81ce915976e
Agusti Bau <agustibau@gmail.com>
2016-11-11 20:39:46 +0100

    scripts i tal suposo

---
+++ Wipeclone/Assets/Scripts/ShipController.js
@@ -0,0 +1,25 @@
+﻿#pragma strict
+
+
+public var speed : int = 5;
+public var torque: int = 5;
+function Start () {
+
+
+}
+
+function FixedUpdate() {
+	var rb = GetComponent.<Rigidbody>();
+	var turn = Input.GetAxis("Horizontal");
+
+	rb.AddTorque(transform.forward * torque * turn);
+	rb.AddTorque(transform.up * torque/10 * turn);
+
+
+//	var up = transform.up + Vector3(turn, turn, 0);
+
+//	rb.MoveRotation(Quaternion.LookRotation(transform.forward, up) );
+
+	var accel = Input.GetAxis("Vertical");
+	rb.AddRelativeForce(0,accel*speed,0);
+}
