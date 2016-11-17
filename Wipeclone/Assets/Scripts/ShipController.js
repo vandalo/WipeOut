6ad599d59bf42b164ffdf3ccc326bee390d6dcfa
Ferran coma rosell <ferrancoma@MacBook-MacBook-Pro-de-Ferran.local>
2016-11-17 13:00:16 +0100

    ship fire

--- Wipeclone/Assets/Scripts/ShipController.js
+++ Wipeclone/Assets/Scripts/ShipController.js
@@ -6,6 +6,10 @@ public var torque: float = 5;
 public var maxVel: int = 255;
 public var rotationPoint: Transform;
 public var maxAngularVelocity: float;
+public var fire: ParticleSystem;
+var projectile : Transform;
+var bulletSpeed : float = 20;
+
 function Start () {
 
 
@@ -16,9 +20,26 @@ function FixedUpdate() {
 	var turn = Input.GetAxis("Horizontal");
 
 	rb.AddTorque(rotationPoint.up * torque * turn);
-	//rb.AddTorque(transform.up * torque/10 * turn);
+
 	rb.maxAngularVelocity = maxAngularVelocity;
 	var accel = Input.GetAxis("Vertical");
+
+	if(Input.GetAxis("Vertical") > 0) fire.Play();
+	else fire.Stop();
+
 	if(rb.velocity.magnitude < maxVel)
 		rb.AddRelativeForce(0,accel*speed,0);
 }
+
+ function Update () {
+     // Put this in your update function
+     /*if (Input.GetButtonDown("Fire1")) {
+ 
+     // Instantiate the projectile at the position and rotation of this transform
+     var clone : Transform;
+     clone = Instantiate(projectile, transform.position, transform.rotation);
+ 
+     // Add force to the cloned object in the object's forward direction
+     clone.rigidbody.AddForce(clone.transform.forward * shootForce);*/
+     }
+ 
